dd331a9d21b6fb8677078494459d702419395bd7
JPLA-DEVELOPER <jpla.developer@gmail.com>
2023-04-11 23:59:19 -0300

    ....

--- routes/rotasVeiculos.js
+++ routes/rotasVeiculos.js
@@ -1,21 +1,22 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router() //importa o recurso router do express e atribue a constante router.
 const Veiculos = require('../models/cadastro');
 
 //CRIANDO ROTAS: -----------------------------------------------------------------------
 //Rota principal
-app.get('/', function(req, res){
+
+router.get('/', function(req, res){
     Veiculos.findAll().then(function(veiculos){ //puxa todos os dados na tabela do bd.
          //console.log(veiculos)
          res.render('home', {veiculos: veiculos})
+         veiculos.push(veiculo)//<!--Partials-->
      }) 
  })
  
  
  
- 
  //ROTA PARA DELETAR UM REGISTRO DA TABELA
- app.get('/deletar/:id', function(req, res){
+router.get('/deletar/:id', function(req, res){
      Veiculos.destroy({where: {'id': req.params.id}}).then(function(){ //condição que deleta os registros pelo id.
          //res.send("Postagem deletada com sucesso!")
          res.render('sucesso_deletar')
@@ -26,7 +27,7 @@ app.get('/', function(req, res){
  
  
  // ROTA PARA EDITAR UM REGISTRO DA TABELA
- app.get('/editar/:id', function(req, res){
+ router.get('/editar/:id', function(req, res){
      Veiculos.findByPk(req.params.id).then(function(veiculo){ //encontra o registro correspondente ao id passado na URL
          res.render('editar', {veiculo: veiculo}) //renderiza a página de edição com as informações do veículo
      }).catch(function(erro){
@@ -35,7 +36,7 @@ app.get('/', function(req, res){
  })
  
  // ROTA PARA ATUALIZAR UM REGISTRO DA TABELA
- app.post('/editar/:id', function(req, res){
+ router.post('/editar/:id', function(req, res){
      Veiculos.update({
          modelo: req.body.modelo,
          ano: req.body.ano
@@ -54,13 +55,13 @@ app.get('/', function(req, res){
  
  
  //ROTA PARA CADASTRO
- app.get('/cadastro', function(req, res){ //nome da rota
+ router.get('/cadastro', function(req, res){ //nome da rota
    
      res.render('formulario') //renderiza o arquivo .handlebars correspondente a rota desejada.
  })
  
  //CRIA ROTA POST COM OS DADOS DO FORMULÁRIO
- app.post('/dados', function(req, res){//Recebe os dados do formulario
+ router.post('/dados', function(req, res){//Recebe os dados do formulario
      Veiculos.create({
          modelo:  req.body.modelo,
          ano: req.body.ano  //pega o dado fornecido no campo do formulario html de nome modelo(BodyParser).
@@ -77,9 +78,35 @@ app.get('/', function(req, res){
  
  
  //CRIANDO UMA NOVA ROTA COM HANDLEBARS PARA O ARQUIVO 404.HANDLEBARS QUE É UMA PAGINA PARA UM ENDEREÇO NÃO EXISTENTE
- app.use((req, res) => {
+ router.use((req, res) => {
      res.status(404).render('404')
    });
 
 
-   //module.exports = router //exporta as rotas para serem usadas pelo index.js
+   
+//-----------------------------------------------------------------------------------------------------
+//Rota para a págian sobre
+//app.get("/sobre", function(req, resp){ 
+//    resp.send("sobre nós!")//Mensagem a ser exibida se o usuário pegar essa rota. Pode fazer diversas coisas com essa rota, não só exibir mensagens.
+//})
+
+//Rota para a págian blog
+//app.get("/blog", function(req, resp){
+//    resp.send("Blog!")
+//})
+
+//ROTAS DINÂMICAS USANDO PARÂMETROS
+//app.get("/:nome/:idade/:sexo", function(req, resp){ //consegue pegar os valores digitados na barra de endereços 
+ //   resp.send("<h2> Seu nome é"+ req.params.nome + "</h2>" + "<h2> Sua idade é"+ req.params.idade + "</h2>" + "<h2> Seu sexo é"+ req.params.sexo + "</h2>")
+//})
+
+
+
+//ROTAS PARA ARQUIVOS HTML
+//app.get("/index", function(req, resp){
+//    resp.sendFile(__dirname + "/html/index.html")//__dirname indica que é o diretório principal do projeto. isso é concatenado com a pasta e o noem do arquivo.
+//})
+//---------------------------------------------------------------------------------------------------------------------
+
+
+   module.exports = router //exporta as rotas para serem usadas pelo index.js
