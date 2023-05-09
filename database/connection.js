c95420cfac9f5a82a79d2512fb2aac97a67d6664
Willian de Oliveira Cardoso <willian.oliveira.cardoso07@aluno.ifce.edu.br>
2023-05-09 16:47:25 -0300

    Login de usuario

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('locadora', 'root', '', {
+const sequelize = new Sequelize('locadora', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
   query:{raw:true}
